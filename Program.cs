d56b7fb137a87744b416df87bb293906a5db8b83
Megyeri Márk Máté <megyko1234@gmail.com>
2026-02-14 13:11:59 +0100

    Darabolás

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿string[] valasztas = new string[3];
+﻿using System;
+using System.IO;
+using System.Text;
+
+string[] valasztas = System.IO.File.ReadAllLines("szavazatok.txt", Encoding.UTF8);
+
+for (int i = 0; i < valasztas.Length; i++)
+{
+    Console.WriteLine(valasztas[i]);
+}
+foreach (var item in valasztas)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine($"A választáson {valasztas.Length} választó vett részt.");
+
+//Hány válsztó vett részt?
+int S = 0 ;
+for (int i = 0; i < valasztas.Length; i++)
+{
+    string[] darab = valasztas[i].Split(' ');
+    S+= int.Parse(darab[1]);
+}
